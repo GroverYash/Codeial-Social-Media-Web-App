c0db7b10b771e31824e36e8dd412298c231cd32d
Yash Grover <yashgrover964@gmail.com>
2021-06-07 15:33:49 +0530

    add sign in, sign up, header ,footer

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express=require('express');
+const cookieParser=require('cookie-parser');
 const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
 app.use(express.static('./assets'));
 app.use(expressLayouts);
 
@@ -11,6 +18,7 @@ app.set('layout extractScripts',true);
 app.use('/',require('./routes'));
 app.set('view engine','ejs');
 app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
