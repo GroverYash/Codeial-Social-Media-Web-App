70c22a58c7de5917f6fe84fa695b385e8644b98f
Yash Grover <yashgrover964@gmail.com>
2021-06-17 14:47:01 +0530

    added flash messages

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -3,17 +3,18 @@ const passport=require('passport');
 const LocalStrategy=require('passport-local').Strategy;
 const User=require('../models/user');
 passport.use(new LocalStrategy({
-    usernameField:'email'
+    usernameField:'email',
+    passReqToCallback:true
 },
-function(email,password,done){
+function(req,email,password,done){
     //find a user and establish identity
     User.findOne({email:email},function(err,user){
         if(err){
-            console.log('error in finding user--> Passport');
+            req.flash('error',err);
             return done(err);
         }
         if(!user || user.password!=password){
-            console.log('Invalid Username/Password');
+            req.flash('error','Invalid Username/Password');
             return done(null,false);
         }
         return done(null,user);
