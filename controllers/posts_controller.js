c3681b484d7fc983c9981285b3a2636d6e1e7da9
Yash Grover <yashgrover964@gmail.com>
2021-06-21 14:44:24 +0530

    Added ajax features for no reload after adding post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,6 +4,7 @@ const Comment=require('../models/comment');
 module.exports.create=async function(req,res){
     try{
         let post=await Post.create({
+            name:req.user.name,
             content:req.body.content,
             user:req.user._id
         });
@@ -30,6 +31,14 @@ module.exports.destroy=async function(req,res){
         if(post.user == req.user.id){
             post.remove();
             await Comment.deleteMany({post:req.params.id});
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    message:"Post Deleted"
+                })
+            }
             req.flash('success','Post and associated comments deleted');
             return res.redirect('back');
         }else{
